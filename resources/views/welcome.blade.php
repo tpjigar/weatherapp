b655410cf21d2b567b50bcd032a683eccb9e47c9
Jigar Patel <tp.jigar@gmail.com>
2020-02-21 15:57:48 +0530

    Title update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=devioce-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Weather App with Laravel, Vuejs, Algolia, Darksky API </title>
     <link rel="stylesheet" href="/css/main.css">
 
     {{-- Algolia css for map only    --}}
